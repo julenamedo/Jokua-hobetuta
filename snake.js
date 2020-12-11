2cb6232ae5bd9a0a816e4c5e1afdcb9bb3a32cdd
julenamedo <72855106+julenamedo@users.noreply.github.com>
2020-12-11 12:15:33 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,11 +1,10 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
-// sugearen pixela sortu
+// box baten dimentsioak erakutsi 32x32 pixel
 const box = 32;
 
-// argazkiak hasieratu
-
+// argazkiak kargatu, fondoa eta sagarra
 const ground = new Image();
 ground.src = "img/ground.png";
 
@@ -13,7 +12,6 @@ const foodImg = new Image();
 foodImg.src = "img/food.png";
 
 // audioak hasieratu
-
 let dead = new Audio();
 let eat = new Audio();
 let up = new Audio();
@@ -28,44 +26,51 @@ right.src = "audio/right.mp3";
 left.src = "audio/left.mp3";
 down.src = "audio/down.mp3";
 
-// sortu sugea
-
+// karratua(sugea deklaratu eta hasieratu)
 let snake = [];
 
-snake[0] = {
-    x : 9 * box,
-    y : 10 * box
+snake[0] = 
+{
+    x : 9 * box, //x ardatzean zein box-etan hasieratzen den
+    y : 10 * box //y ardatzean zein box-etan hasieratzen den
 };
 
-// sortu janaria
-
-let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
+// JANARIA
+//Hasierako sagarra zer posiziotan marraztuko den kalkulatzen du Math.random-ek
+let food = 
+{
+    x : Math.floor(Math.random()*17+1) * box, //Kasu honetan, 17box eta 1box-aren artean. X ARDATZEAN
+    y : Math.floor(Math.random()*15+3) * box // Kasu honetan, 15box eta 3box-aren artean. Y ARDATZEAN
 }
 
-// sortu score
-
+// score markagailua sortu, margotu eta 0ra hasieratu
 let score = 0;
 
-//sugea kontrolatu
-
-let d;
+//sugearen kontrola
+let d; // d aldagai globala deklaratu.
 
 document.addEventListener("keydown",direction);
 
-function direction(event){
+function direction(event)
+{
     let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
+    if( key == 37 && d != "RIGHT")
+    {
         left.play();
         d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
+    }
+    else if(key == 38 && d != "DOWN")
+    {
         d = "UP";
         up.play();
-    }else if(key == 39 && d != "LEFT"){
+    }
+    else if(key == 39 && d != "LEFT")
+    {
         d = "RIGHT";
         right.play();
-    }else if(key == 40 && d != "UP"){
+    }
+    else if(key == 40 && d != "UP")
+    {
         d = "DOWN";
         down.play();
     }
@@ -81,67 +86,79 @@ function collision(head,array){
     return false;
 }
 
-// dena marraztu canvasean
-
-function draw(){
+// Canvasean margotzen dena agertuko da hemen
+function draw()
+{
     
-    ctx.drawImage(ground,0,0);
+    ctx.drawImage(ground,0,0); //Lurraren argazkia kordenadatan ipini
     
-    for( let i = 0; i < snake.length ; i++){
+    for( let i = 0; i < snake.length ; i++)
+    {
         ctx.fillStyle = ( i == 0 )? "green" : "green";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);//karratuaren barrukoa 
         
-        ctx.strokeStyle = "black";
+        ctx.strokeStyle = "black";//karratua kalkulatu
         ctx.strokeRect(snake[i].x,snake[i].y,box,box);
     }
     
-    ctx.drawImage(foodImg, food.x, food.y);
+    ctx.drawImage(foodImg, food.x, food.y); //Sagarra margotuko du food.x eta food.y kordenadatan, hauek bi funtziotan kalkulatzen dira.
+    //hasieratzeko funtzioan eta sagar bat jaterakoan beste sagar berri bat jartzeko kalkulatzen duena.
     
-    // sugearen azken burua non zegoen
+    // Karratuaren x eta y ardatzetako kordenadak gordeko dira array-an.
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
     
-    // noranzkoa
+    // noranzkoa zehaztu
     if( d == "LEFT") snakeX -= box;
     if( d == "UP") snakeY -= box;
     if( d == "RIGHT") snakeX += box;
     if( d == "DOWN") snakeY += box;
     
-    // sugea janaria jaten badu
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
+    // JANARIA JAN 
+    // karratuaren x eta y ardatzako posizioa eta janariarena berdina badira = puntu bat+ eta beste sagar berri baten posizioa kalkulatzen du.
+    if(snakeX == food.x && snakeY == food.y)
+    {
+        score++; //scorea igo
         eat.play();
-        food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*15+3) * box
+        food = 
+        {
+         x : Math.floor(Math.random()*17+1) * box, //Math.random-ek posizioa aleatorio bat kalkulatzen du ondorengo posiziotan. 17box eta 1box-aren artean. X ARDATZEAN
+            y : Math.floor(Math.random()*15+3) * box //""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 15box eta 3box-aren artean. Y ARDATZEAN
         }
-        // ez da kentzen ezer
-    }else{
+        // sugearen isatsa ez da kentzen
+    }
+    else
+    {
         // sugearen azken pixela kendu
         snake.pop();
     }
     
-    // sugeari gorputz bat gehitu
-    
-    let newHead = {
+    // gehitu buru berri bat, baina gorputza ez da handitzen
+    let newHead = 
+    {
         x : snakeX,
         y : snakeY
     }
     
     // galdu duzu
     
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
-        clearInterval(game);
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake))
+    {
+        clearInterval(interbaloa); //funtzio bat gelditzeko, kasu honetan gure funtzioaren interbaloa gelditu egiten du.
+        ctx.fillStyle = "black"; //Game over-en kolorea
+        ctx.font = "80px Changa one"; //letra mota eta tamaina
+        ctx.fillText('GAME OVER!', 1.75*box, 10.7*box); //zer ipiniko duen eta zer posiziotan
         dead.play();
     }
     
-    snake.unshift(newHead);
+    snake.unshift(newHead); //Array-ko hasieran balore berriak ipintzeko, hau da, newHeaden kalkulatutako kordenadak arrayko hasieran ipini.
     
-    ctx.fillStyle = "green";
-    ctx.font = "45px Changa one";
-    ctx.fillText(score,2*box,1.6*box);
+    //score-ko puntuak zer koloretan, fuentetan eta posiziotan agertuko diren
+    ctx.fillStyle = "white"; // kolorea
+    ctx.font = "45px Changa one"; //zer letra mota eta tamaina
+    ctx.fillText(score,2*box,1.6*box); // filltext = printf ==> score-a zer posiziotan margotuko duen adierazten du. 
 }
 
-// draw funtzioa egin interbalo batean
 
-let game = setInterval(draw,100);
+// funtzioaren interbaloa (zenbat aldiz errefreskatzen da)
+let interbaloa = setInterval(draw,100);
